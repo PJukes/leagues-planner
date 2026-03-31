9dc3deea42800c3dc0eb706757181f52e838d541
Phil Jukes <phil.jukes@gmail.com>
2026-03-31 20:44:09 +0100

    Loads of changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './templates/**/*.html',
+    "./templates/*.html",
+    "./templates/**/*.html",
+    "./**/templates/**/*.html",
+    "./frontend/src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},

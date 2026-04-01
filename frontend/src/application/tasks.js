669193a04d3ed5cb2b2235e37f05a18f9c68ea3f
Phil Jukes <phil.jukes@gmail.com>
2026-04-01 21:25:35 +0100

    Added skill and relics

--- frontend/src/application/tasks.js
+++ frontend/src/application/tasks.js
@@ -1,16 +1,44 @@
 export function taskManager() {
+    const RELIC_LIST = [
+        ["Endless Harvest", "Barbarian Gathering", "Abundance"],
+        ["Woodsman",],
+        ["Evil Eye"],
+        ["Conniving Clues"],
+        ["Nature's Accord"],
+        ["Culling Spree"],
+        ["Minion", "Flask of Fervour"]
+    ];
+    const RELIC_POINTS_TIER = [0, 750, 1500, 2500, 3500, 5000, 10000, 15000];
+
     return {
         showModal: false,
-        tasks: [{name: 'Open the Leagues Menu', points: 10, id: 1}],
+        actions: [],
+        totalPoints: 0,
+        totalTasks: 0,
+        taskList: [],
+        relicList: RELIC_LIST,
+        relicSelection: [],
+        currentRelicTier: 0,
+        selectedTask: null,
         init() {
             window.addEventListener('add-task', (event) => {
                 console.log("Adding task", event);
                 this.openModal();
             });
+            window.addEventListener('add-skill', (event) => {
+                console.log("Adding skill", event);
+                this.openModal("skill-list-template");
+            });
+            fetch('http://127.0.0.1:8002/planner/task-list/')
+                .then(res => res.json())
+                .then(data => {
+                    this.taskList = data["tasks"];
+                    console.log(this.taskList);
+                });
         },
-        openModal() {
+        openModal(content="task-list-template") {
             this.showModal = true;
-            const template = document.getElementById('task-list-template');
+            const template = document.getElementById(content);
             const modalContent = document.getElementById('modalContent');
 
             if (!template || !modalContent) return;
@@ -23,11 +51,88 @@ export function taskManager() {
         closeModal() {
             this.showModal = false;
         },
-        addTask(task) {
-            this.tasks.push(task);
+        addTask(task_key) {
+            console.log("Adding task", task_key);
+            const task = this.taskList.find(task => task.key === task_key);
+            if (task) {
+                task.type = "task";
+                task.selected = false;
+                this.actions.push(task);
+                this.totalTasks += 1;
+                this.totalPoints += task.league_points;
+                task.cumulativePoints = this.totalPoints;
+            }
+            this.closeModal();
         },
-        removeTask(taskId) {
-            this.tasks = this.tasks.filter(task => task.id !== taskId);
+        selectTask(taskKey) {
+            if (this.selectedTask != null) {
+                this.selectedTask.selected = false;
+            }
+            if (this.selectedTask === taskKey) {
+                this.selectedTask = null;
+                return;
+            }
+            this.selectedTask = this.actions.find(task => task.key === taskKey);
+            this.selectedTask.selected = true;
         },
+        removeTask(taskKey) {
+            this.actions = this.actions.filter(task => task.key !== taskKey);
+            const removedTask = this.taskList.find(task => task.key === taskKey);
+            if (removedTask) {
+                this.totalTasks -= 1;
+                this.totalPoints -= removedTask.league_points;
+            }
+        },
+        addRelic(relicKey) {
+            console.log("Adding relic", relicKey);
+            const relic = {
+                key: relicKey,
+                name: relicKey,
+                type: "relic"
+            };
+            this.actions.push(relic);
+            this.relicSelection.push(relicKey);
+            this.closeModal();
+        },
+        getSkillExperience(action) {
+            // Loop through all actions up to the current action and calculate experience
+            // for all skills
+            let experience = 0;
+            for (const act of this.actions) {
+                if (act.key === action.key) break;
+                experience += act.experience;
+            }
+            return experience;
+        },
+        canPickRelic() {
+            if (this.currentRelicTier === 0 && this.relicSelection.length === 0) {
+                return true;
+            }
+            const totalPoints = this.totalPoints;
+            for(let i=0; i<RELIC_POINTS_TIER.length; i++) {
+                if (totalPoints >= RELIC_POINTS_TIER[i] && this.currentRelicTier < i) {
+                    return true;
+                }
+            }
+            return false;
+        },
+        pickRelic() {
+            if (this.canPickRelic()) {
+                this.openModal('relic-list-template');
+            }
+        },
+        addSkillAction(skill, method, quantity) {
+            console.log("Adding skill action", skill, method, quantity);
+            const skillAction = {
+                key: `${skill}-${method}-${quantity}`,
+                name: skill,
+                method: method,
+                quantity: quantity,
+                type: "skill"
+            };
+            this.actions.push(skillAction);
+            this.closeModal();
+        }
+
     };
 }
--- leaguesplanner/planner/urls.py
+++ leaguesplanner/planner/urls.py
@@ -14,10 +14,5 @@ urlpatterns = [
     path("<int:plan_id>/task-library/", views.task_library_data, name="task_library_data"),
     path("<int:plan_id>/update/", views.plan_update, name="plan_update"),
     # Tasks
-    path("<int:plan_id>/tasks/", views.task_create, name="task_create"),
-    path("<int:plan_id>/tasks/reorder/", views.task_reorder, name="task_reorder"),
-    path("<int:plan_id>/tasks/<int:task_id>/", views.task_detail, name="task_detail"),
-    # Tiers
-    path("<int:plan_id>/tiers/", views.tier_create, name="tier_create"),
-    path("<int:plan_id>/tiers/<int:tier_id>/", views.tier_detail, name="tier_detail"),
+    path("task-list/", views.task_list, name="task_list")
 ]
